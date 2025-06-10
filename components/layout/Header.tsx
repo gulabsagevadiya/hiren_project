f926ad15d2467ce6635f5d6ba7ec524294c519c0
gulabsagevadiya <gulabp20@gmail.com>
2025-06-10 20:02:20 +0530

    a

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -23,7 +23,7 @@ export function Header() {
       <div className="container mx-auto px-4">
         <div className="flex h-20 items-center justify-between">
           {/* Logo */}
-          <Link href="/" className="flex items-center space-x-2">
+          <Link href="/" className="flex items-center space-x-2" prefetch={false}>
             <Shield className="h-8 w-8 text-blue-600" />
             <div className="hidden sm:block">
               <div className="text-xl font-bold text-gray-900">Inspectra</div>
@@ -38,6 +38,7 @@ export function Header() {
                 key={item.name}
                 href={item.href}
                 className="text-gray-700 hover:text-blue-600 font-medium transition-colors"
+                prefetch={false}
               >
                 {item.name}
               </Link>
@@ -46,12 +47,12 @@ export function Header() {
 
           {/* CTA Button */}
           <div className="hidden md:flex items-center space-x-4">
-            <Link href="tel:+917600869646" className="flex items-center text-gray-700 hover:text-blue-600">
+            <Link href="tel:+917600869646" className="flex items-center text-gray-700 hover:text-blue-600" prefetch={false}>
               <Phone className="h-4 w-4 mr-2" />
               <span className="font-medium">+91 7600869646</span>
             </Link>
             <Button asChild>
-              <Link href="/contact">Request Quote</Link>
+              <Link href="/contact" prefetch={false}>Request Quote</Link>
             </Button>
           </div>
 
@@ -64,7 +65,7 @@ export function Header() {
             </SheetTrigger>
             <SheetContent side="right" className="w-80">
               <div className="flex flex-col space-y-6 mt-6">
-                <Link href="/" className="flex items-center space-x-2" onClick={() => setIsOpen(false)}>
+                <Link href="/" className="flex items-center space-x-2" onClick={() => setIsOpen(false)} prefetch={false}>
                   <Shield className="h-6 w-6 text-blue-600" />
                   <div>
                     <div className="font-bold text-gray-900">Inspectra</div>
@@ -79,6 +80,7 @@ export function Header() {
                       href={item.href}
                       className="text-gray-700 hover:text-blue-600 font-medium py-2"
                       onClick={() => setIsOpen(false)}
+                      prefetch={false}
                     >
                       {item.name}
                     </Link>
@@ -86,12 +88,12 @@ export function Header() {
                 </nav>
                 
                 <div className="flex flex-col space-y-4 pt-4 border-t">
-                  <Link href="tel:+917600869646" className="flex items-center text-gray-700">
+                  <Link href="tel:+917600869646" className="flex items-center text-gray-700" prefetch={false}>
                     <Phone className="h-4 w-4 mr-2" />
                     <span>+91 7600869646</span>
                   </Link>
                   <Button asChild onClick={() => setIsOpen(false)}>
-                    <Link href="/contact">Request Quote</Link>
+                    <Link href="/contact" prefetch={false}>Request Quote</Link>
                   </Button>
                 </div>
               </div>
